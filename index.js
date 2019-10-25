2e862956c2e925df34c812b860d741dcf1bf8e5c
Caleb F <calebsf2000@gmail.com>
2019-10-24 20:44:11 -0400

    Added a post option so others can post

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 const express = require('express')
+const bodyParser = require('body-Parser')
 const teams = require('./teams.json')
 
 const app = express()
 
-app.get('./teams', (request, response) => {
+app.get('/teams', (request, response) => {
 	response.send(teams)
 })
 
-app.get('/teams/:x', (request, response) => {
-	const x = request.params.x
+app.get('/teams/:identifier', (request, response) => {
+	const identifier = request.params.identifier
 
 	const matchingTeams = teams.filter(() => {
-		return teams.id === number(x)
+		return teams.id === number(identifier) || teams.abreviation === identifier.toUpperCase()
 	})
+
 	if (matchingTeams.length) {
 		response.send(matchingTeams)
 	} else {
 		response.sendStatus(404)
 	}
 })
+app.use(bodyParser.json())
+app.post('/teams', (request, response) => {
+	const { id, location, mascot, abbreviation, conference, division } = request.body
 
+	if (!id || !location || !mascot || !abbreviation || !conference || !division) {
+		response.status(400).send('The following attributes are required:id, location, mascot,abbreviation,conference,division ')
+	}
+
+	teams.push({ id, location, mascot, abbreviation, conference, division })
+
+	const newTeam = { id, location, mascot, abbreviation, conference, division }
+
+	response.status(201).send(newTeam)
+})
 app.all('*', (request, response) => {
 	response.sendStatus(404)
 })
+
 
 
 const server = app.listen(9090, () => { console.log('Listening on port 9090') })
